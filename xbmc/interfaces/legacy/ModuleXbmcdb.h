476096f8e76e2260748e93ac6b9d171414b1509b
Violet Red <red.violet1@gmx.com>
2015-05-12 21:23:44 +0200

    Expand Python interface
    - Export MediaType constants.
    - Simpler wrapper for Python interface.

--- xbmc/interfaces/legacy/ModuleXbmcdb.h
+++ xbmc/interfaces/legacy/ModuleXbmcdb.h
@@ -45,7 +45,8 @@ namespace XBMCAddon
      * example:
      *   - xbmcplugin.addItemtoDb("musicvideo", listitem)
      */
-    int addItemtoVideoDb(const String& mediatype, const XBMCAddon::xbmcgui::ListItem* listItem);
+    int addItemtoVideoDb(const String& mediatype,
+        const XBMCAddon::xbmcgui::ListItem* listItem);
 
     /**
      * deleteItemfromVideoDb(path) -- Remove all references to item from database
@@ -57,17 +58,30 @@ namespace XBMCAddon
     void deleteItemfromVideoDb(const String& path);
 
     /**
+     * getVideoItemsByWhere(where, join, fields, order, group) -- Run a SQL query in videodb
+     * where        : string - Basic SQL statement
+     *
+     * example:
+     *   - xbmcplugin.getMoviesByWhere(MEDIA_TYPE_MOVIE, "title like '%mymovie%')
+     */
+    std::vector<XBMCAddon::xbmcgui::ListItem*> getVideoItemsByWhere(
+        const String& mediaType, const String& SQLWhere,
+        const String& SQLJoin = emptyString,
+        const String& SQLFields = emptyString,
+        const String& SQLOrder = emptyString,
+        const String& SQLGroup = emptyString);
+
+    /**
      * getMoviesByWhere(where, join, fields, order, group) -- Run a SQL query in videodb
      * where        : string - Basic SQL statement
      *
      * example:
      *   - xbmcplugin.getMoviesByWhere("name like '%mymovie%')
      */
-    std::vector<XBMCAddon::xbmcgui::ListItem*> getMoviesByWhere(const String& SQLWhere,
-        const String& SQLJoin = emptyString,
-        const String& SQLFields = emptyString,
-        const String& SQLOrder = emptyString,
-        const String& SQLGroup = emptyString);
+    std::vector<XBMCAddon::xbmcgui::ListItem*> getMoviesByWhere(
+        const String& SQLWhere, const String& SQLJoin = emptyString,
+        const String& SQLFields = emptyString, const String& SQLOrder =
+            emptyString, const String& SQLGroup = emptyString);
 
     /**
      * getMusicVideosByWhere(where, join, fields, order, group) -- Run a SQL query in videodb
@@ -76,10 +90,22 @@ namespace XBMCAddon
      * example:
      *   - xbmcplugin.getMusicVideosByWhere("name like '%mymovie%')
      */
-    std::vector<XBMCAddon::xbmcgui::ListItem*> getMusicVideosByWhere(const String& SQLWhere,
-        const String& SQLJoin = emptyString,
-        const String& SQLFields = emptyString,
-        const String& SQLOrder = emptyString,
-        const String& SQLGroup = emptyString);
+    std::vector<XBMCAddon::xbmcgui::ListItem*> getMusicVideosByWhere(
+        const String& SQLWhere, const String& SQLJoin = emptyString,
+        const String& SQLFields = emptyString, const String& SQLOrder =
+            emptyString, const String& SQLGroup = emptyString);
+
+    SWIG_CONSTANT2(std::string, MEDIA_TYPE_NONE, MediaTypeNone);
+    SWIG_CONSTANT2(std::string, MEDIA_TYPE_MUSIC, MediaTypeMusic);
+    SWIG_CONSTANT2(std::string, MEDIA_TYPE_ARTIST, MediaTypeArtist);
+    SWIG_CONSTANT2(std::string, MEDIA_TYPE_ALBUM, MediaTypeAlbum);
+    SWIG_CONSTANT2(std::string, MEDIA_TYPE_SONG, MediaTypeSong);
+    SWIG_CONSTANT2(std::string, MEDIA_TYPE_VIDEO, MediaTypeVideo);
+    SWIG_CONSTANT2(std::string, MEDIA_TYPE_VIDEO_COLLECTION,MediaTypeVideoCollection);
+    SWIG_CONSTANT2(std::string, MEDIA_TYPE_MUSIC_VIDEO, MediaTypeMusicVideo);
+    SWIG_CONSTANT2(std::string, MEDIA_TYPE_MOVIE, MediaTypeMovie);
+    SWIG_CONSTANT2(std::string, MEDIA_TYPE_TVSHOW, MediaTypeTvShow);
+    SWIG_CONSTANT2(std::string, MEDIA_TYPE_SEASON, MediaTypeSeason);
+    SWIG_CONSTANT2(std::string, MEDIA_TYPE_EPISODE, MediaTypeEpisode);
   }
 }
